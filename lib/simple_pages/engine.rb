d6328012acfb934d96fc4f2f3115b7f4df08a50d
Tse-Ching Ho <tsechingho@gmail.com>
2012-12-14 17:25:07 +0800

    setup config of generators

--- lib/simple_pages/engine.rb
+++ lib/simple_pages/engine.rb
@@ -1,6 +1,11 @@
 module SimplePages
   class Engine < ::Rails::Engine
     isolate_namespace SimplePages
+
+    config.generators.stylesheet_engine :sass
+    config.generators.integration_tool :rspec
+    config.generators.test_framework :rspec
+    config.generators.fixture_replacement :factory_girl, dir: 'spec/factories'
   end
 end
 
