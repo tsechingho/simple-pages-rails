ba0000928d865c94832d66f5900013acc6c1ad53
Tse-Ching Ho <tsechingho@gmail.com>
2012-12-18 12:01:18 +0800

    inject codes to application controller

--- lib/generators/simple_pages/install_generator.rb
+++ lib/generators/simple_pages/install_generator.rb
@@ -14,6 +14,22 @@ module SimplePages
         template 'simple_pages.rb', 'config/initializers/simple_pages.rb'
       end
 
+      def mount_routes
+        insert_into_file 'config/routes.rb', after: "routes.draw do\n" do
+          "  mount SimplePages::Engine, at: '/'\n"
+        end
+      end
+
+      def inject_application_controller
+        insert_into_file 'app/controllers/application_controller.rb', after: /class ApplicationController < ActionController::Base\n/ do
+          <<-CODE
+  include DeviseExt
+  include CanCanExt
+  include SimplePages::Controllers::PageLayoutAt
+          CODE
+        end
+      end
+
       def append_user_mixin
         user_model = "app/models/#{options[:user_model].downcase}.rb"
         unless File.exist? user_model
@@ -27,12 +43,6 @@ module SimplePages
 
   include SimplePages::Models::PageAuthor
           CODE
-        end
-      end
-
-      def mount_routes
-        insert_into_file 'config/routes.rb', after: "routes.draw do\n" do
-          "  mount SimplePages::Engine, at: '/'\n"
         end
       end
     end
--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  include DeviseExt
+  include CanCanExt
+  include SimplePages::Controllers::PageLayoutAt
   protect_from_forgery
 end
