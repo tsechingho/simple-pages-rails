1a0ffd70d1eb1632251b4db05c8e950dbd1c689d
Tse-Ching Ho <tsechingho@gmail.com>
2013-03-18 10:54:32 +0800

    check url of page before submit

--- app/assets/javascripts/simple_pages/pages.js.coffee
+++ app/assets/javascripts/simple_pages/pages.js.coffee
@@ -2,3 +2,17 @@ $ ->
   $('.pages .chzn-select').chosen
     allow_single_deselect: true
     no_results_text: 'No results matched'
+
+  $('.pages')
+    .on 'focusout', '#page_url', ->
+      if $(this).val().length > 0
+        if ! /^([\w\-]{3,})$/.test($(this).val())
+          alert('Invalid URL!')
+          $(this).val('')
+    .on 'submit', '.simple_form', (event) ->
+      url = $('#page_url')
+      if url.val().length > 0
+        if ! /^([\w\-]{3,})$/.test(url.val())
+          alert('Invalid URL!')
+          url.val('')
+          return false
--- lib/simple_pages/models/page_url.rb
+++ lib/simple_pages/models/page_url.rb
@@ -6,20 +6,14 @@ module SimplePages
       extend ActiveSupport::Concern
       included do
         attr_accessible :url
-        validates :url, format: { with: /[\w\d\-_]{3,}/ }, allow_blank: true
+        validates :url, format: { with: /^([\w\-]{3,})$/ }, allow_blank: true
         acts_as_url :title, sync_url: true, only_when_blank: true
       end
 
       module ClassMethods
         def find_or_create_by_url(attrs)
           url = attrs.delete :url
-          page = where(url: url).first
-          if page.nil?
-            page = create(attrs)
-            page.url = url
-            page.save
-          end
-          page
+          where(url: url).first_or_create(attrs)
         end
       end
 
